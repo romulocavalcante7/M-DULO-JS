6c852c682fb780b0065c3fed7d2684ca1d768d06
Romulo.Cavalcante <romulogomescavalcante7@gmail.com>
2023-10-22 21:43:03 -0300

    first commit

---
+++ Dominando a manipulação do DOM/script.js
@@ -0,0 +1,17 @@
+//como ler através das tags
+
+var p = document.getElementsByTagName('p')
+
+// p
+
+//formula de html para o js
+//alert(p[3].innerHTML);
+
+//formula de js para html
+//p[0].innerHTML = 'Manipulando via JS';
+
+//para todas as tags P
+/*for(var i = 0; i < p.length; i++){
+    p[i].innerHTML= 'Manipulando via JS -'+ i;
+}*/
+
